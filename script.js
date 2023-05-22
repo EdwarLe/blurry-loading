085d7efbe773f340a99df7c36b4b8650171c7439
Edwar León <edwar.leon.m@gmail.com>
2023-05-21 21:50:29 -0500

    se agrega el intervalo de tiempo del contador

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const load__text = document.getElementById('load__text')
 
 var contar = 0;
 
-window.onload = function () {
-    load__text.innerHTML = contar;
-    if (contar == 100) {
-        load__text.style.opacity = '0'
-    } else {
-        load__text.style.opacity = '100%'
-        contar++
-        setTimeout(contar, 5000)
+let time = setInterval(() => {
+    contar++;
+    load__text.textContent = contar + '%';
+
+    if (contar === 100) {
+        clearInterval(time)
     }
+}, 40)
+
+window.onload = function () {
+    load__text.style.transition = '6s';
+    load__text.style.opacity = '0';    
     bg_blur.style.transition = '5s';
     bg_blur.style.filter = 'blur(0px)';
 }
